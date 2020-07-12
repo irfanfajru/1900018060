bb97fc7fdb7a9424e312ea912f43fc53f54dc646
Muhammad Irfan Fajru Ramadhan <62731237+irfanfajru@users.noreply.github.com>
2020-07-12 08:34:33 +0700

    Update akses.php

--- akses.php
+++ akses.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Terimakasih</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<div class="con">
+<div class="konten">
 <?php 
 	$nama=$nim=$kelas=$gender=$motiv="";
 	if (isset($_POST["nama"]) && isset($_POST["nim"]) && isset($_POST["kelas"]) && isset($_POST["jk"]) && isset($_POST["motivasi"])) {
@@ -14,14 +17,29 @@
 		$motiv=$_POST["motivasi"];
 	}
 	if (!empty($nama)&&!empty($nim)&&!empty($kelas)&&!empty($gender)&&!empty($motiv)) {
-		echo "<h1>Selamat Datang </h1>".strtoupper($nama)."<br>";
-		echo "<p>NIM : .$nim</p>";
-		echo "<p>Kelas : .$kelas</p>";
-		echo "<p>jenis kelamin : .$gender</p>";
-		echo "<p>Motivasi masuk jurusan <br>teknik informatika : .$motiv</p>";
+		$fp=fopen("daftar.txt","a+");
+        fputs($fp,"$nim\n$nama\n$kelas\n$gender\n$motiv\n");
+        fclose($fp);
+		echo "<h1>Terimakasih</h1>";
+		echo "Nama : ".strtoupper($nama)."<br>";
+		echo "<p>NIM : $nim</p>";
+		echo "<p>Kelas : $kelas</p>";
+		echo "<p>jenis kelamin : $gender</p>";
+		echo "<p>Alasan Mendaftar menjadi asprak: $motiv</p>";
 	}else{
 		echo "silahkan masukkan data dengan lengkap";
 	}
 ?>
+<br><br>
+<center>
+<table>
+	<tr>
+		<td><a href="index.html" class="tombol">Kembali</a></td>
+		<td><a href="table.php" class="tombol">Lihat Pendaftar</a></td>
+	</tr>
+</table>
+</center>
+</div>
+</div>
 </body>
 </html>
